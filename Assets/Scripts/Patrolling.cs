494b675078a3524df917636626bb04c4173b3586
tahasar <taha_asar100@hotmail.com>
2023-05-06 15:47:51 +0300

    3 map temeli, whitechapelsiz

---
+++ Assets/Scripts/Patrolling.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrolling : MonoBehaviour
+{
+
+    public Transform[] points;
+
+    private int current;
+
+    private float speed = 2;
+    public NavMeshAgent agent;
+    public int targetNumber;
+    private float targetDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = 0;
+        targetNumber = Random.Range(0, points.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetDistance = Vector2.Distance(points[targetNumber].position, transform.position);
+        
+        if (targetDistance > 0.5f)
+        {
+            agent.SetDestination(points[targetNumber].position);
+            //transform.position = Vector3.MoveTowards(transform.position, points[current].position, speed * Time.deltaTime);
+            //transform.LookAt(points[targetNumber].position);
+
+            
+        }
+        else
+        {
+            targetNumber = Random.Range(0,points.Length);
+            //current = (current + 1) % points.Length;
+        }
+    }
+}
